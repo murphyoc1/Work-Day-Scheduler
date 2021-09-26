de2d508ff90bed9ad2bd8d01a1fc5a8bb5e2cc61
murphyoc1 <murphyoc1@gmail.com>
2021-09-26 11:51:44 -0400

    updated id's for clearity

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 // save button
   $('.saveBtn').on('click', function() {
     var value = $(this)
-      .siblings('.description')
+      .siblings('.event')
       .val();
-    // adding task from the class description in textbox in html to local storage
+    // adding task from the class event in textbox in html to local storage
     var time = $(this)
       .parent()
       .attr('id');
@@ -45,16 +45,16 @@ $(document).ready(function() {
 
   // take from local storage and display back in the textbox
 
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-  $('#hour-18 .description').val(localStorage.getItem('hour-18'));
+  $('#hour-9 .event').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .event').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .event').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .event').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .event').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .event').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .event').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .event').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .event').val(localStorage.getItem('hour-17'));
+  $('#hour-18 .event').val(localStorage.getItem('hour-18'));
 
   $('#currentDay').text(moment().format('dddd, MMMM Do'));
 });
